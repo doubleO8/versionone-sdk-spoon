a4f0f00521a17339f1964e3ce02855cecee11865
Wolfgang <wb008@hdm-stuttgart.de>
2016-02-06 09:45:12 +0100

    fix: shadow build-in name 'input'; cleanup variable names

--- v1pysdk/string_utils.py
+++ v1pysdk/string_utils.py
@@ -1,23 +1,28 @@
-def split_attribute(input):
+def split_attribute(attribute_string):
     """
-properly split apart attribute strings, even when they have sub-attributes declated between [].
-:param input:the attribute string to split
-:return: dict containing the elements of the top level attribute string.
-Sub-attribute strings between '[]'s are appended to their parent, without processing, even if they contain '.'
-"""
+    Properly split apart attribute strings, even when they have sub-attributes
+    declated between [].
+    Sub-attribute strings between '[]'s are appended to their parent, without
+    processing, even if they contain '.'
+
+    :param attribute_string:the attribute string to split
+    :return: dict containing the elements of the top level attribute string.
+    """
     ret = []
     # zero means we are not inside square brackets
-    squareBrackets = 0
-    lastIndex = 0
-    for i in range(len(input)):
-        if input[i] == '[':
-            squareBrackets += 1
-        elif input[i] == ']':
-            squareBrackets -= 1
-        elif input[i] == '.' and squareBrackets == 0:
-            ret.append(input[lastIndex:i])
-            lastIndex = i + 1
+    square_brackets = 0
+    last_index = 0
+    a_len = len(attribute_string)
+
+    for i in range(a_len):
+        if attribute_string[i] == '[':
+            square_brackets += 1
+        elif attribute_string[i] == ']':
+            square_brackets -= 1
+        elif attribute_string[i] == '.' and square_brackets == 0:
+            ret.append(attribute_string[last_index:i])
+            last_index = i + 1
 
     # last element
-    ret.append(input[lastIndex:])
+    ret.append(attribute_string[last_index:])
     return ret
