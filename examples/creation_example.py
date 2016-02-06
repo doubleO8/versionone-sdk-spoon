cda092e01a545cb4e00e5b1953f6a66d784e451d
Wolfgang <wb008@hdm-stuttgart.de>
2016-02-06 10:10:35 +0100

    code cleanup; executable bit and python shebang added

--- examples/creation_example.py
+++ examples/creation_example.py
@@ -1,17 +1,14 @@
-
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from v1pysdk import V1Meta
 
 v1 = V1Meta()
 
 new_story = v1.Story.create(
-                      Name = "New Story", 
-                      Scope = v1.Scope(1002),
-                      )
-
+        Name="New Story",
+        Scope=v1.Scope(1002),
+)
 
 new_story.Owners = list(v1.Member.where(Name="Admin"))
 
 v1.commit()
-
-
--- examples/query_fluent_example.py
+++ examples/query_fluent_example.py
@@ -1,16 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from v1pysdk import V1Meta
 
-
 v1 = V1Meta()
-
 
 for t in v1.AssetType.select('Name').where(Name='Story'):
     print t
 
-
 for s in v1.Story.select('Name'):
     print s.CreateDate  # fetched on demand
     print s.Name
-
-
-
