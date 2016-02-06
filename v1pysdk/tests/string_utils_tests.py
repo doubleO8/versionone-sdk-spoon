6b417b360dad40a990c87595e938b23aad90a955
Wolfgang <wb008@hdm-stuttgart.de>
2016-02-06 09:55:14 +0100

    hackfix: enforce testing of v1pysdk module in parent folder

--- v1pysdk/tests/string_utils_tests.py
+++ v1pysdk/tests/string_utils_tests.py
@@ -1,4 +1,9 @@
+import sys
+import os
 import unittest
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../.."))
+
 from v1pysdk.string_utils import split_attribute
 
 
@@ -22,36 +27,36 @@ class TestStringUtils(unittest.TestCase):
                           split_attribute('[testing.a.sentence.is].difficult'))
         self.assertEquals(['[testing.][a.sentence.is.difficult]'],
                           split_attribute(
-                              '[testing.][a.sentence.is.difficult]'))
+                                  '[testing.][a.sentence.is.difficult]'))
         self.assertEquals(
                 ['[testing]', '[a]', '[sentence]', '[is]', '[difficult]'],
                 split_attribute('[testing].[a].[sentence].[is].[difficult]'))
         self.assertEquals(['testing', '[[a.sentence.]is]', 'difficult'],
                           split_attribute(
-                              'testing.[[a.sentence.]is].difficult'))
+                                  'testing.[[a.sentence.]is].difficult'))
         self.assertEquals(["History[Status.Name='Done']"],
                           split_attribute("History[Status.Name='Done']"))
         self.assertEquals(["ParentMeAndUp[Scope.Workitems.@Count='2']"],
                           split_attribute(
-                              "ParentMeAndUp[Scope.Workitems.@Count='2']"))
+                                  "ParentMeAndUp[Scope.Workitems.@Count='2']"))
         self.assertEquals(["Owners", "OwnedWorkitems[ChildrenMeAndDown=$]",
                            "@DistinctCount"],
                           split_attribute(
-                              "Owners.OwnedWorkitems[ChildrenMeAndDown=$].@DistinctCount"))
+                                  "Owners.OwnedWorkitems[ChildrenMeAndDown=$].@DistinctCount"))
         self.assertEquals(["Workitems[ParentAndUp[Scope=$].@Count='1']"],
                           split_attribute(
-                              "Workitems[ParentAndUp[Scope=$].@Count='1']"))
+                                  "Workitems[ParentAndUp[Scope=$].@Count='1']"))
         self.assertEquals(
                 ["RegressionPlan", "RegressionSuites[AssetState!='Dead']",
                  "TestSets[AssetState!='Dead']", "Environment",
                  "@DistinctCount"]
                 , split_attribute(
-                "RegressionPlan.RegressionSuites[AssetState!='Dead'].TestSets[AssetState!='Dead'].Environment.@DistinctCount"))
+                        "RegressionPlan.RegressionSuites[AssetState!='Dead'].TestSets[AssetState!='Dead'].Environment.@DistinctCount"))
         self.assertEquals(["Scope", "ChildrenMeAndDown",
                            "Workitems:Story[ChildrenMeAndDown.ToDo.@Sum!='0.0']",
                            "Estimate", "@Sum"]
                           , split_attribute(
-                "Scope.ChildrenMeAndDown.Workitems:Story[ChildrenMeAndDown.ToDo.@Sum!='0.0'].Estimate.@Sum"))
+                    "Scope.ChildrenMeAndDown.Workitems:Story[ChildrenMeAndDown.ToDo.@Sum!='0.0'].Estimate.@Sum"))
 
 
 if __name__ == '__main__':
